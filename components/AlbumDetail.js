8c1f0c7c8a42c83180f7eb2bf2e3f9dee2fcd39e
Govind <shremish@indiana.edu>
2017-06-16 18:47:44 -0400

    Displaying the image with texts in row

--- components/AlbumDetail.js
+++ components/AlbumDetail.js
@@ -1,19 +1,23 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
 
 import Card from './Card';
 import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({album}) => {
+
+	const {title, artist, thumbnail_image} = album;
+	const {headerTextStyle, thumbnail_imageStyle} = styles;
+
 	return (
 		<Card>
 		  <CardSection>
 		   <View>
-
+            <Image source={{uri: thumbnail_image}}/>
 		   </View>
 		   <View>
-		     <Text>{props.album.title}</Text>
-		     <Text>{props.album.artist}</Text>
+		     <Text>{title}</Text>
+		     <Text>{artist}</Text>
 		   </View>
 		 </CardSection>
 		</Card>
@@ -21,5 +25,15 @@ const AlbumDetail = (props) => {
 
 };
 	
+   const styles = {
+   		headerTextStyle: {
+   			flexDirection: 'row',
+   			justifyContent: 'space-around'
+   		},
+   		thumbnail_imageStyle: {
+   			
+   		}
+
+   };
 
 export default AlbumDetail;
